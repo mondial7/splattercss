47555d59c27c1e211544588baa28bd15ed4f6ca8
Marco Mondini || Knopfy <marco@dataagency.it>
2019-11-25 20:13:22 +0200

    🐶 hooks & hooks

--- build/splattercss.js
+++ build/splattercss.js
@@ -31,7 +31,7 @@
   /**
    * Define the module as empty object
    */
-  let SplatterCss = {};
+  const SplatterCss = {};
 
   /**
    * Parse a given css string
@@ -41,25 +41,25 @@
    * @param String css
    * @return Array {Object}
    */
-  SplatterCss.parse = function(css) {
+  SplatterCss.parse = function parse(css) {
     // return value
-    let list = [];
+    const list = [];
     // When the string is empty or undefined, no need to evaluate
     if (!css || css.length === 0) return list
     // loop around parenthesis `}`
-    let pieces = css.split('}');
-    for (let i=0; i<pieces.length; i++) {
-      let y = pieces[i].split('{');
+    const pieces = css.split('}');
+    for (let i = 0; i < pieces.length; i += 1) {
+      const y = pieces[i].split('{');
       // check if { has been found
-      if (y.length === 1) {
-        continue
-      } else {
+      if (y.length !== 1) {
         // get left and right of `{`
-        let selector = y[0].trim(), rules = y[1].trim();
+        const selector = y[0].trim();
+        const rules = y[1].trim();
         // selector cannot be empty 0.0
-        if (selector.length === 0) continue
-        // add to resulting list
-        list.push({ selector: selector, rules: rules });
+        if (selector.length !== 0) {
+          // add to resulting list
+          list.push({ selector, rules });
+        }
       }
     }
     return list
@@ -72,25 +72,27 @@
    * @param String dom
    * @return String new dom with injected styles
    */
-  SplatterCss.inject = function(list, dom) {
-    // if arguments are missing or empty return the empty string
-    if (list === undefined || list.lenght === 0 || !dom) {
-      return ''
+  SplatterCss.inject = function inject(list, dom) {
+    // when arguments are missing or empty
+    // return the dom or empty string to silently fail
+    // without throwing exceptions
+    if (!list || !dom) {
+      return dom || ''
     }
     // transform the dom string in HTMLElements in order to navigate it
-    let container = document.createElement('div');
+    const container = document.createElement('div');
     container.insertAdjacentHTML('beforeend', dom);
     // loop through the list and add styles to dom
-    for (let i = 0; i < list.length; i++) {
-      container.querySelectorAll(list[i].selector).forEach(function(el) {
-        let currentStyles = el.getAttribute('style');
-        let newStyles = !!currentStyles
+    for (let i = 0; i < list.length; i += 1) {
+      container.querySelectorAll(list[i].selector).forEach((el) => {
+        const currentStyles = el.getAttribute('style');
+        const newStyles = currentStyles
           ? `${currentStyles};${list[i].rules}`
           : list[i].rules;
         el.setAttribute('style', newStyles);
       });
     }
-    // return the string rapresentation of the styled dom
+    // return the string representation of the styled dom
     return container.innerHTML
   };
 
@@ -101,7 +103,7 @@
    * @param String dom
    * @return String styled dom
    */
-  SplatterCss.mesh = function(css, dom) {
+  SplatterCss.mesh = function mesh(css, dom) {
     return this.inject(this.parse(css), dom)
   };
 
